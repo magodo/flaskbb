abd9e16563d3f82d9e3a3c1be3964542fdd283b5
magodo <wztwcy@gmail.com>
2017-05-30 20:40:23 +0800

    add encoding line

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
 from flaskbb import create_app
-from flaskbb.configs.production import ProductionConfig
+#from flaskbb.configs.production import ProductionConfig
+from flaskbb.configs.development import DevelopmentConfig
 
-flaskbb = create_app(config=ProductionConfig())
+#flaskbb = create_app(config=ProductionConfig())
+flaskbb = create_app(config=DevelopmentConfig())
